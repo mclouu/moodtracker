af2eef1534d36079b127384811f267f1f035392b
mathieu romain <rmathieu13@gmail.com>
2018-02-02 01:31:44 +0100

    fix bug after change pc + add variable mood for detect when it's swip for change smiley

--- app/src/main/java/com/mathieu/romain/moodtracker/MainActivity.java
+++ app/src/main/java/com/mathieu/romain/moodtracker/MainActivity.java
@@ -3,6 +3,7 @@ package com.mathieu.romain.moodtracker;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,27 +15,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        imgSmiley = findViewById(R.id.img_smiley);
 
-        ImageView imgSmiley = findViewById(R.id.img_smiley);
-
-        imgSmiley.setOnTouchListener(new SwipeDirectionListener() {
-
-            TextView tv = findViewById(R.id.tv);
+        RelativeLayout layout = findViewById(R.id.main_activity_layout);
 
 
-            public void onUpSwipe(float value) {
-                tv.setText("UpSwipe");
-            }
+            layout.setOnTouchListener(new SwipeDirectionListener() {
 
-            public void onDownSwipe(float value) {
-                tv.setText("DownSwipe");
-            }
-        });
+                TextView tv = findViewById(R.id.tv);
+                int humeur = 3;
+
+
+                public void onUpSwipe(float value) {
+                    if (humeur < 4) {
+                        humeur++;
+                    }
+                    tv.setText("UpSwipe " + humeur);
+                }
+
+                public void onDownSwipe(float value) {
+                    tv.setText("DownSwipe " + humeur);
+                    if (humeur > 0) {
+                        humeur--;
+                    }
+                }
+            });
+        }
 
 
     }
-
-
-}
 
--- app/src/main/java/com/mathieu/romain/moodtracker/SwipeDirectionListener.java
+++ app/src/main/java/com/mathieu/romain/moodtracker/SwipeDirectionListener.java
@@ -9,13 +9,9 @@ import android.view.View;
 
 public class SwipeDirectionListener implements View.OnTouchListener {
 
-    float deltaX;
     float deltaY;
-    float maxValX;
     float maxValY;
-    float firstTouchX;
     float firstTouchY;
-    float currentX;
     float currentY;
     float SWIPE_THRESHOLD = 10.0f;
 
@@ -25,40 +21,32 @@ public class SwipeDirectionListener implements View.OnTouchListener {
 
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
+
                 //Register the first touch on TouchDown and this should not change unless finger goes up.
-                firstTouchX = event.getX();
                 firstTouchY = event.getY();
-                maxValX = 0.0f;
                 maxValY = 0.0f;
+
                 //As the event is consumed, return true
                 result = true;
                 break;
 
             case MotionEvent.ACTION_MOVE:
+
                 //CurrentX/Y are the continues changing values of one single touch session. Change
                 //when finger slides on view
-                currentX = event.getX();
                 currentY = event.getY();
-                //setting the maximum value of X or Y so far. Any deviation in this means a  change of direction so reset the firstTouch value to last known max value i.e MaxVal X/Y.
-                if (maxValX < currentX) {
-                    maxValX = currentX;
-                } else {
-                    firstTouchX = maxValX;
-                    maxValX = 0.0f;
-                }
 
+                //setting the maximum value of X or Y so far. Any deviation in this means a  change of direction so reset the firstTouch value to last known max value i.e MaxVal X/Y.
                 if (maxValY < currentY) {
                     maxValY = currentY;
                 } else {
                     firstTouchY = maxValY;
                     maxValY = 0.0f;
                 }
-                //DeltaX/Y are the difference between current touch and the value when finger first touched screen.
-                //If its negative that means current value is on left side of first touchdown value i.e Going left and
-                //vice versa.
-                deltaX = currentX - firstTouchX;
-                deltaY = currentY - firstTouchY;
 
+                //DeltaX/Y are the difference between current touch and the value when finger first touched screen.
+                //If its negative that means current value is on down side of first touchdown value i.e Going down
+                deltaY = currentY - firstTouchY;
 
                 //vertical swipe
                 if (Math.abs(deltaY) > SWIPE_THRESHOLD) {
@@ -70,15 +58,12 @@ public class SwipeDirectionListener implements View.OnTouchListener {
                         onUpSwipe(currentY);
                     }
                 }
-
                 result = true;
                 break;
 
             case MotionEvent.ACTION_UP:
                 //Clean UP
-                firstTouchX = 0.0f;
                 firstTouchY = 0.0f;
-
                 result = true;
                 break;
 
@@ -91,7 +76,7 @@ public class SwipeDirectionListener implements View.OnTouchListener {
     }
 
 
-    //constructeur
+    //CONSTRUCTEUR
 
     public void onUpSwipe(float value) {
 
