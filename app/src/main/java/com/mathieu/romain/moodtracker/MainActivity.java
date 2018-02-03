dbffb9fb460caf40d827abd04862cc891b391b8c
mathieu romain <rmathieu13@gmail.com>
2018-02-03 12:51:27 +0100

    add swipe backgroundColor

--- app/src/main/java/com/mathieu/romain/moodtracker/MainActivity.java
+++ app/src/main/java/com/mathieu/romain/moodtracker/MainActivity.java
@@ -29,16 +29,18 @@ public class MainActivity extends AppCompatActivity {
 
         layout.setOnTouchListener(new SwipeDirectionListener(MainActivity.this) {
             ImageView imgSmiley = findViewById(R.id.img_smiley);
+            RelativeLayout layout = findViewById(R.id.main_activity_layout);
             TextView tv = findViewById(R.id.tv);
 
             int tableauImg[] = {R.drawable.smileysad, R.drawable.smileydisappointed, R.drawable.smileynormal, R.drawable.smileyhappy, R.drawable.smileysuperhappy};
-
+            int tableauBackground[] = {R.color.Background_sad,R.color.Background_disappointed,R.color.Background_normal,R.color.Background_happy,R.color.Background_superhappy};
 
             public void onUpSwipe() {
                 if (mood != 4) {
                     mood++;
                 }
                 imgSmiley.setImageResource(tableauImg[mood]);
+                layout.setBackgroundResource(tableauBackground[mood]);
                 tv.setText("UpSwipe " + mood);
 
             }
@@ -48,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
                     mood--;
                 }
                 imgSmiley.setImageResource(tableauImg[mood]);
+                layout.setBackgroundResource(tableauBackground[mood]);
                 tv.setText("DownSwipe " + mood);
 
             }
