5529e750042cb6cae4638ec49d70eeadeda06f19
mathieu romain <rmathieu13@gmail.com>
2018-02-04 21:49:44 +0100

    create java file mooddata + variable and constructor

---
+++ app/src/main/java/com/romain/mathieu/moodtracker/MoodData.java
@@ -0,0 +1,23 @@
+package com.romain.mathieu.moodtracker;
+
+/**
+ * Created by Romain on 04/02/2018.
+ */
+
+public class MoodData {
+    String time;
+    String message;
+    int messageIcons;
+    int sizeCard;
+    int colorCard;
+
+
+    public MoodData(String time, String message,  int colorCard) {
+        this.time = time;
+        this.message = message;
+        this.messageIcons = messageIcons;
+        this.sizeCard = sizeCard;
+        this.colorCard = colorCard;
+    }
+
+}
