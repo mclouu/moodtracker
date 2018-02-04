e4a4c1f115d9630842682fec6429de9e9f1647a9
mathieu romain <rmathieu13@gmail.com>
2018-02-04 21:43:43 +0100

    i worked on recyclerView, it's not finish but we can see a progression

--- app/src/main/java/com/romain/mathieu/moodtracker/HistoryActivity.java
+++ app/src/main/java/com/romain/mathieu/moodtracker/HistoryActivity.java
@@ -3,18 +3,51 @@ package com.romain.mathieu.moodtracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class HistoryActivity extends AppCompatActivity {
+
+    private ArrayList<MoodData> moodData;
+            RecyclerView recyclerView;
+
+            CardView cardView;
+            String message;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
 
-        final RecyclerView recyclerView =  findViewById(R.id.recyclerView);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(new MyAdapter());
+
+
+        recyclerView =  findViewById(R.id.recyclerView);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(llm);
+
+        initializeData();
+
+        MyAdapter adapter = new MyAdapter(moodData);
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void initializeData() {
+        moodData = new ArrayList<>();
+        moodData.add(new MoodData("Hier", "trop bon la pizza", R.color.color_superhappy));
+        moodData.add(new MoodData("Avant-hier", "trop bon la pizza", R.color.color_superhappy));
+        moodData.add(new MoodData("Il y a un jours", "trop bon la pizza", R.color.color_superhappy));
+        moodData.add(new MoodData("Il y a deux jours", "trop bon la pizza", R.color.color_superhappy));
+        moodData.add(new MoodData("Il y a trois jours", "trop bon la pizza", R.color.color_superhappy));
+        moodData.add(new MoodData("Il y a quatre jours", "trop bon la pizza", R.color.color_superhappy));
+        moodData.add(new MoodData("Il y a cinq jours", "trop bon la pizza", R.color.color_superhappy));
+        moodData.add(new MoodData("Il y a six jours", "trop bon la pizza", R.color.color_superhappy));
+        moodData.add(new MoodData("Il y a une semaine", "trop bon la pizza", R.color.color_superhappy));
     }
 }
--- app/src/main/java/com/romain/mathieu/moodtracker/MyAdapter.java
+++ app/src/main/java/com/romain/mathieu/moodtracker/MyAdapter.java
@@ -1,25 +1,73 @@
 package com.romain.mathieu.moodtracker;
 
+
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
 
 /**
  * Created by Romain on 03/02/2018.
  */
 
-public class MyAdapter extends RecyclerView.Adapter {
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.HistoryViewHolder> {
+
+    public List<MoodData> mdatas;
+
+    public MyAdapter(List<MoodData> datas){
+        this.mdatas = datas;
+    }
+
+
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+    public HistoryViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.row, parent, false);
+        HistoryViewHolder viewHolder = new HistoryViewHolder(v);
+        return viewHolder;
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(HistoryViewHolder holder, int position) {
+        holder.textViewTime.setText(mdatas.get(position).time);
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return this.mdatas.size();
+    }
+
+    public class HistoryViewHolder extends RecyclerView.ViewHolder {
+
+        CardView cardView;
+        TextView textViewTime;
+        ImageButton messageIcons;
+        String message;
+
+        public HistoryViewHolder(final View itemView) {
+            super(itemView);
+            cardView = itemView.findViewById(R.id.cardview);
+            textViewTime = itemView.findViewById(R.id.textview_time);
+            messageIcons = itemView.findViewById(R.id.message_icons);
+
+            messageIcons.setClickable(true);
+            message = "j'aime l'argent";
+            messageIcons.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    Toast.makeText(itemView.getContext(),message, Toast.LENGTH_SHORT).show();
+
+                }
+            });
+
+        }
     }
 }
