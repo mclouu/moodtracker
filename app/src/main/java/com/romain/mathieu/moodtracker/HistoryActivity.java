62e75b7de6738588aa65f8d2682965605753954c
Romain Mathieu <rmathieu13@gmail.com>
2018-02-11 14:18:01 +0100

    migration hashMap and ArrayList to MainActivity and add save/get arrayList with SharedPreferences Gson

--- app/src/main/java/com/romain/mathieu/moodtracker/HistoryActivity.java
+++ app/src/main/java/com/romain/mathieu/moodtracker/HistoryActivity.java
@@ -1,22 +1,15 @@
 package com.romain.mathieu.moodtracker;
 
-
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.DisplayMetrics;
 
-import java.util.ArrayList;
-import java.util.Hashtable;
 
 public class HistoryActivity extends AppCompatActivity {
 
-    private ArrayList<MoodData> moodData;
-    Hashtable widthMood;
-    Hashtable colorMood;
-    RecyclerView recyclerView;
 
+    RecyclerView recyclerView;
 
 
     @Override
@@ -25,37 +18,13 @@ public class HistoryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_history);
 
 
-
         recyclerView = findViewById(R.id.recyclerView);
         LinearLayoutManager llm = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(llm);
 
-        initializeData();
 
-        MyAdapter adapter = new MyAdapter(moodData);
+        MyAdapter adapter = new MyAdapter(MainActivity.moodData);
         recyclerView.setAdapter(adapter);
     }
 
-    private void initializeData() {
-        DisplayMetrics metrics = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(metrics);
-
-        widthMood = new Hashtable();
-        widthMood.put(4, (float) metrics.widthPixels); // is supperhappy
-        widthMood.put(3, (float) metrics.widthPixels * 0.75f); // is happy
-        widthMood.put(2, (float) metrics.widthPixels * 0.50f); // is normal
-        widthMood.put(1, (float) metrics.widthPixels * 0.37f); // is disappointed
-        widthMood.put(0, (float) metrics.widthPixels * 0.25f); //is sad
-
-        colorMood = new Hashtable();
-        colorMood.put(4, R.color.color_superhappy);
-        colorMood.put(3, R.color.color_happy);
-        colorMood.put(2, R.color.color_normal);
-        colorMood.put(1, R.color.color_disappointed);
-        colorMood.put(0, R.color.color_sad);
-
-
-        moodData = new ArrayList<>();
-        moodData.add(new MoodData("Hier", SharedPreferencesUtils.getMessage(this), (int) colorMood.get(SharedPreferencesUtils.getMood(this)), (Float) widthMood.get(SharedPreferencesUtils.getMood(this))));
-    }
 }
--- app/src/main/java/com/romain/mathieu/moodtracker/MainActivity.java
+++ app/src/main/java/com/romain/mathieu/moodtracker/MainActivity.java
@@ -1,10 +1,13 @@
 package com.romain.mathieu.moodtracker;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -12,8 +15,16 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
-import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+import static com.romain.mathieu.moodtracker.SharedPreferencesUtils.MY_FILE;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -23,22 +34,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     ImageButton btnAddMessage;
     //test variable
     Button btnMidNight;
-    TextView tvMessage;
-    TextView tvMood;
-
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        SharedPreferencesUtils.saveMood(this, mood);
-    }
+    // array varible
+    public static ArrayList<MoodData> moodData;
+    Hashtable widthMood;
+    Hashtable colorMood;
+    Hashtable position;
+    int time = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        moodData = new ArrayList<>();
 
-
+        getArrayList();
 
 
         mood = SharedPreferencesUtils.getMood(this);
@@ -60,6 +69,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnMidNight.setOnClickListener(this);
 
         swipe();
+
+
+        initializeData();
 
 
     }
@@ -130,15 +142,67 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             startActivity(myIntent);
         } else if (btnMidNight == view) {
             Toast.makeText(getApplication(), "il est minuit héhé", Toast.LENGTH_SHORT).show();
-            tvMessage = findViewById(R.id.tv_test_message);
-            tvMood = findViewById(R.id.tv_test_mood);
-            tvMessage.setText(SharedPreferencesUtils.getMessage(this));
-            tvMood.setText(String.valueOf(SharedPreferencesUtils.getMood(this)));
+            SharedPreferencesUtils.saveMood(this, mood);
+            moodData.add(new MoodData((String) position.get(time), SharedPreferencesUtils.getMessage(this), (int) colorMood.get(SharedPreferencesUtils.getMood(this)), (Float) widthMood.get(SharedPreferencesUtils.getMood(this))));
 
 
         }
     }
 
+    private void initializeData() {
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+
+        widthMood = new Hashtable();
+        widthMood.put(4, (float) metrics.widthPixels); // is supperhappy
+        widthMood.put(3, (float) metrics.widthPixels * 0.75f); // is happy
+        widthMood.put(2, (float) metrics.widthPixels * 0.50f); // is normal
+        widthMood.put(1, (float) metrics.widthPixels * 0.37f); // is disappointed
+        widthMood.put(0, (float) metrics.widthPixels * 0.25f); //is sad
+
+        colorMood = new Hashtable();
+        colorMood.put(4, R.color.color_superhappy);
+        colorMood.put(3, R.color.color_happy);
+        colorMood.put(2, R.color.color_normal);
+        colorMood.put(1, R.color.color_disappointed);
+        colorMood.put(0, R.color.color_sad);
+
+        position = new Hashtable();
+        position.put(6, "Il y a une semaine");
+        position.put(5, "Il y a 6 jours");
+        position.put(4, "Il y a 5 jours");
+        position.put(3, "Il y a 4 jours");
+        position.put(2, "Il y a 3 jours");
+        position.put(1, "Avant-hier");
+        position.put(0, "Hier");
+
+
+    }
+
+    public void saveArrayList() {
+        SharedPreferences sharedPreferences = getSharedPreferences(MY_FILE, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(moodData);
+        editor.putString("moodDataList", json);
+        editor.apply();
+    }
+
+    public void getArrayList() {
+        SharedPreferences sharedPreferences = getSharedPreferences(MY_FILE, Context.MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = sharedPreferences.getString("moodDataList", null);
+        Type type = new TypeToken<ArrayList<MoodData>>() {
+        }.getType();
+        moodData = gson.fromJson(json, type);
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        saveArrayList();
+    }
 }
 
 
