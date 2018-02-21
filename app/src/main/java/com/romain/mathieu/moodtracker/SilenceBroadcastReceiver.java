150606ea409b4fa92fa3a05d3dea31d41e5fd6c1
romain <rmathieu13@gmail.com>
2018-02-21 14:41:25 +0100

    test to Receive (AlarmManager)

---
+++ app/src/main/java/com/romain/mathieu/moodtracker/SilenceBroadcastReceiver.java
@@ -0,0 +1,19 @@
+package com.romain.mathieu.moodtracker;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+/**
+ * Created by romain on 20/02/2018.
+ */
+
+public class SilenceBroadcastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        Toast.makeText(context, "il est minuit héhé", Toast.LENGTH_SHORT).show();
+
+    }
+}
