2f036d96fe9d3fa96db74899d07bb9d0249d77e1
romain <rmathieu13@gmail.com>
2018-02-22 23:15:10 +0100

    Move method getArrayList in SharedPreferencesUtils

--- app/src/main/java/com/romain/mathieu/moodtracker/Controller/HistoryActivity.java
+++ app/src/main/java/com/romain/mathieu/moodtracker/Controller/HistoryActivity.java
@@ -1,22 +1,13 @@
 package com.romain.mathieu.moodtracker.Controller;
 
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-import com.romain.mathieu.moodtracker.Model.MoodData;
+import com.romain.mathieu.moodtracker.Model.SharedPreferencesUtils;
 import com.romain.mathieu.moodtracker.R;
 import com.romain.mathieu.moodtracker.View.MyAdapter;
-
-import java.lang.reflect.Type;
-import java.util.ArrayList;
-
-import static com.romain.mathieu.moodtracker.Model.SharedPreferencesUtils.MY_FILE;
 
 
 public class HistoryActivity extends AppCompatActivity {
@@ -31,7 +22,7 @@ public class HistoryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_history);
 
 
-        getArrayList();
+        SharedPreferencesUtils.getArrayList(this);
 
 
         recyclerView = findViewById(R.id.recyclerView);
@@ -44,13 +35,5 @@ public class HistoryActivity extends AppCompatActivity {
         recyclerView.setAdapter(adapter);
     }
 
-    public void getArrayList() {
-        SharedPreferences sharedPreferences = getSharedPreferences(MY_FILE, Context.MODE_PRIVATE);
-        Gson gson = new Gson();
-        String json = sharedPreferences.getString("moodDataList", null);
-        Type type = new TypeToken<ArrayList<MoodData>>() {
-        }.getType();
-        MainActivity.moodData = gson.fromJson(json, type);
-    }
 
 }
