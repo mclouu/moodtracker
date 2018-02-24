0538d2ef66380b075536fdf032b1bc76106c21a8
romain <rmathieu13@gmail.com>
2018-02-24 23:51:48 +0100

    adding condition => if containsMood = false (app is not launched today) than create a black cardView with message "empty mood"

--- app/src/main/java/com/romain/mathieu/moodtracker/Model/MyBroadcastReceiver.java
+++ app/src/main/java/com/romain/mathieu/moodtracker/Model/MyBroadcastReceiver.java
@@ -4,9 +4,10 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
-import static com.romain.mathieu.moodtracker.Controller.MainActivity.colorMood;
+import com.romain.mathieu.moodtracker.R;
+
 import static com.romain.mathieu.moodtracker.Controller.MainActivity.moodData;
-import static com.romain.mathieu.moodtracker.Controller.MainActivity.widthMood;
+import static com.romain.mathieu.moodtracker.Controller.MainActivity.tableBackgroundColor;
 
 /**
  * Created by romain on 20/02/2018
@@ -16,27 +17,36 @@ public class MyBroadcastReceiver extends BroadcastReceiver {
 
     /* This method is executed at midnight every day
 
+     * tableWidthMood is a table with the screen width which I multiply by a ratio
+
      * It will create an arrayList with the data wanted by the user
      * Param 1 = the date where mood is recorded.
      * Param 2 = retrieves the message saved in sharedpreferences
-     * Param 3 = retrieves the color in Hashtable colorMood (go to initializeData() in Mainactivity)
+     * Param 3 = retrieves the color in table colorMood (go to initializeData() in Mainactivity)
                * with the mood value saved in my sharedPreferences
-     * Param 4 = same than param 3 but for the hashtable widthMood
+     * Param 4 = same than param 3 but for the table widthMood
 
-      * Save the arrayList */
+     * if containsMood = false (app is not launched today) than create a black cardView with message "empty mood"
+
+      * save the arrayList
+      * reset data */
     @Override
     public void onReceive(Context context, Intent intent) {
-        System.out.println("onReceive OK !");
-        moodData.add(new MoodData(" ", SharedPreferencesUtils.getMessage(context), colorMood.get(SharedPreferencesUtils.getMood(context)), widthMood.get(SharedPreferencesUtils.getMood(context))));
-        System.out.println("ArrayList créer");
+
+        float tableWidthMood[] = {SharedPreferencesUtils.getWidth(context) * 0.25f, SharedPreferencesUtils.getWidth(context) * 0.37f, SharedPreferencesUtils.getWidth(context) * 0.50f, SharedPreferencesUtils.getWidth(context) * 0.75f, SharedPreferencesUtils.getWidth(context)};
+
+        if (SharedPreferencesUtils.containsMood(context)) {
+            moodData.add(new MoodData(" ", SharedPreferencesUtils.getMessage(context), tableBackgroundColor[SharedPreferencesUtils.getMood(context)], tableWidthMood[SharedPreferencesUtils.getMood(context)]));
+        } else {
+            //Todo peut etre afficher la couleur du texte en blanc ?
+            moodData.add(new MoodData(" ", "Aucune humeur enregistré", R.color.color_empty, SharedPreferencesUtils.getWidth(context)));
+        }
+
         SharedPreferencesUtils.saveArrayList(context);
-        System.out.println("Save OK !");
-        //resetMood(context);
-        //System.out.println("reset OK !");
-//
+        resetMood(context);
     }
 
-    // resets mood to 3 to have the default  smiley in the next day
+    // resets mood and message in sharedPreferences
     public void resetMood(Context context) {
         SharedPreferencesUtils.removeMood(context, SharedPreferencesUtils.MY_FILE, SharedPreferencesUtils.KEY_MOOD);
         SharedPreferencesUtils.removeMood(context, SharedPreferencesUtils.MY_FILE, SharedPreferencesUtils.KEY_MESSAGE);
